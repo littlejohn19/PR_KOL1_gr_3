82112aad62ca8a20e94807bf5db21b6aaa42a61b
WojciechSutkowski <wojciechsutkowskii@gmail.com>
2021-04-09 14:01:17 +0200

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<nav><h1>Nawigacja</h1><a href="index.html">Strona Główna</a> <a href="kontakt.html">Kontakt</a> </nav> <a href="formularz.html">Formularz</a>'
+
+document.querySelector('.nav-bar').innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,13 @@
+let kwadracik = document.querySelector('.box');
+let button = document.querySelector('.button');
+
+button.addEventListener('click', function() {
+	kwadracik.style.backgroundColor = "yellow";
+});
+
+
+let button2 = document.querySelector('.button2');
+
+button2.addEventListener('click', function() {
+    document.body.style.backgroundColor = "beige";
+});
